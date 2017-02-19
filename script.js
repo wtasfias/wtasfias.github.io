e3a68bed5ad24ac1ec488aa426217bcc76111c3c
wtasfias <wtasfias@gmail.com>
2017-02-19 08:08:48 -0800

    .-.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-alert("Hello!");
+var name;
 
-function buttonFunction() {
-    var x;
-    if (confirm("Click a button!") == true) {
-        x = "you clicked ok!"
-    } else {
-        x = "You clicked cancel!"
+function startButton () {
+    if (confirm("Disclaimer: we are not responsible for any autism or cancer obtained from this website") == true) {
+        name = prompt("Please enter your name", "bryan choy");
+        window.location.assign("page2.html");
     }
-    document.getElementById("test").innerHTML = x;
+}
+
+function nextButton (a) {
+    var nextSlide = "page".concat(a, ".html");
+    window.location.assign(nextSlide);
 }
