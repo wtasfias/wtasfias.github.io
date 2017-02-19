61dfd35abec419af71b46dc629d4ed7b4985df1c
wtasfias <wtasfias@gmail.com>
2017-02-19 08:02:19 -0800

    new script!!!

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function startButton() {
+var name;
+
+function startButton () {
     if (confirm("Disclaimer: we are not responsible for any autism or cancer obtained from this website") == true) {
-        window.location.assign("test.html");
+        name = prompt("Please enter your name", "bryan choy");
+        window.location.assign("page2.html");
     }
+}
+
+function nextButton (a) {
+    var nextSlide = "page".concat(a, ".html");
+    window.location.assign(nextSlide);
 }
